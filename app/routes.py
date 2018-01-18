0cac9508aca3498e447ad4af686b2bd426ce0223
Rhys Powell <RhysPowell@fico.com>
2018-01-18 20:07:49 +0000

    Move to chapter 3

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,8 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'rhys' }
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' = user['username'] + '''!</h1>
-    </body>
-</html>'''
+    return render_template('index.html', user=user)
